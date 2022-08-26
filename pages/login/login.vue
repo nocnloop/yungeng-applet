a09726b9eedf6cca58a100d7ad7a8e5d4cbd2d51
cuizi <8659048+cuizi-cn@user.noreply.gitee.com>
2022-08-26 13:27:27 +0800

    登录页面逻辑修改

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -9,9 +9,9 @@
           :loading="isLoading"
           type="primary"
           shape="circle"
-          text="点击获取用户信息"
+          text="点击进行用户注册"
           openType="getPhoneNumber"
-          @getphonenumber="decryptPhoneNumber"
+          @getphonenumber="registerUser"
         >
         </u-button>
       </view>
@@ -23,9 +23,7 @@
 </template>
 
 <script>
-import { getCarInfo } from "../../api/car.js"
-import { login, saveWxUser, wxuserregister } from "../../api/login.js"
-import { getUserInfo, getWeChatUserInfo } from "../../api/user.js"
+import { login, wxuserregister } from "../../api/login.js"
 export default {
   data() {
     return {
@@ -34,7 +32,6 @@ export default {
       encryptedData: "",
       userInfo: {},
       isLoading: false,
-      isShowGetUserInfoButton: false,
       isNotify: false
     }
   },
@@ -52,45 +49,6 @@ export default {
       // eslint-disable-next-line
       wx.exitMiniProgram()
     },
-    async onGetUserInfo(res) {
-      try {
-        this.isLoading = true
-        await this.getUserProfile()
-        await this.sendCode() // 在向后台发送加密信息之前保险起见向后台发送code
-        await this.saveUserInfo()
-        await this.noNeedSyncUserInfoAction("getUserInfo")
-        uni.reLaunch({ url: "../index/index" })
-      } catch (e) {
-        console.log(e)
-      } finally {
-        this.isLoading = false
-      }
-    },
-
-    getUserProfile() {
-      return new Promise((resolve, reject) => {
-        uni.getUserProfile({
-          desc: "用于完善会员资料",
-          success: (res) => {
-            this.iv = res.iv
-            this.encryptedData = res.encryptedData
-            this.userInfo = res.userInfo
-            resolve()
-          },
-          fail: (e) => {
-            reject(e)
-          }
-        })
-      })
-    },
-
-    // 保存用户信息
-    async saveUserInfo() {
-      await saveWxUser({
-        iv: this.iv,
-        encryptedData: this.encryptedData
-      })
-    },
 
     getCode() {
       return new Promise((resolve, reject) => {
@@ -107,31 +65,16 @@ export default {
       })
     },
 
-    async sendCode() {
-      try {
-        uni.showLoading()
-        await this.getCode()
-        const result = await login({ code: this.code })
-        this.$store.dispatch("app/tokenAction", result.data.accessToken)
-      } catch (error) {
-        console.log(error)
-      } finally {
-        uni.hideLoading()
-      }
-    },
-
     async login() {
       try {
         uni.showLoading()
         await this.getCode()
         const result = await login({ code: this.code })
-        console.log(result)
-        // needSyncUserInfo 1 表示后台还未存储了用户的微信个人信息，需要进行用户授权
-        // const needSyncUserInfo = result.data.needSyncUserInfo
-        // this.$store.dispatch("app/tokenAction", result.data.accessToken)
-        // needSyncUserInfo ? (this.isShowGetUserInfoButton = true) : await this.noNeedSyncUserInfoAction()
+        const { openID } = result.data
+        if (openID) {
+          uni.reLaunch({ url: "../index/index" })
+        }
       } catch (e) {
-        console.log(e)
         this.$refs.uNotify.show({
           type: "error",
           message: "错误，请下拉刷新重试",
@@ -144,41 +87,14 @@ export default {
       }
     },
 
-    async noNeedSyncUserInfoAction(type) {
-      await this.getUserInfo()
-      await this.getWeChatUserInfo()
-      await this.getCarInfo()
-      type != "getUserInfo" && uni.reLaunch({ url: "../index/index" })
-    },
-
-    async getUserInfo() {
-      const result = await getUserInfo()
-      this.$store.dispatch("app/userAction", result.data)
-    },
-
-    async getWeChatUserInfo() {
-      const result = await getWeChatUserInfo()
-      this.$store.dispatch("app/weChatUserAction", result.data)
-    },
-
-    async getCarInfo() {
-      const result = await getCarInfo()
-      this.$store.dispatch("app/carAction", result.data)
-    },
-
     /**
      * @description: 获取手机号的回调
      * @param {*} e
      * @return {*}
      */
-    async decryptPhoneNumber(e) {
+    async registerUser(e) {
       try {
         this.isLoading = true
-        // const errorMsg = e.detail.errMsg
-        // if (errorMsg == "getPhoneNumber:ok") {
-        //   const { code, encryptedData, iv } = e.detail
-        //   await wxuserregister({ code, encryptedData, iv })
-        // }
         await this.getCode()
         await wxuserregister({ code: this.code, encryptedData: "", iv: "" })
       } catch (error) {
